610aa41d1e75a0ffd691ccb5d997b16458bc2ed9
kyDoleuc04 <46973452+kyDoleuc04@users.noreply.github.com>
2019-03-06 13:46:37 -0500

    Updated from Brython Server: 3/6/2019 1:46:40 PM

--- distribution.py
+++ distribution.py
@@ -39,6 +39,7 @@ Notice about this example:
 import string
 alphabet = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
 str = input("Please enter a string of text (the bigger the better): ")
+print('The distribution of characters in "'+str+'" is: ')
 str = str.lower()
 
 letters = []
